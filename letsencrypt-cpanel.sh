f68e8e299a099663c840d0af9f121904771ce909
Trey Brister <webstandardcss@gmail.com>
2015-12-11 23:55:24 +0000

    USERNAME

--- letsencrypt-cpanel.sh
+++ letsencrypt-cpanel.sh
@@ -2,13 +2,13 @@
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
 
 if [ ! $# == 2 ]; then
-  echo "Usage: $0 USER DOMAIN"
+  echo "Usage: $0 USERNAME DOMAIN"
   exit
 fi
 
-export USER=$1
+export USERNAME=$1
 export DOMAIN=$2
 
-/root/letsencrypt/letsencrypt-auto --text certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
+/root/letsencrypt/letsencrypt-auto --text certonly --renew-by-default --webroot --webroot-path /home/${USERNAME}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
 ~
 /root/installssl.pl ${DOMAIN} 
