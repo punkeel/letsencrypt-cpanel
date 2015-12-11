78ef6171dbadab46b7d0e63bed0cacf4c96f95b6
Trey Brister <webstandardcss@gmail.com>
2015-12-11 23:08:15 +0000

    debug #4 again

--- letsencrypt-cpanel-first.sh
+++ letsencrypt-cpanel-first.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
-
 if [ $# == 1 ] || [ $# == 2 ] || [ $# == 3 ]; then
-  test $(userdomains ${USER}|wc -l) -ne 1 &&\
+  export USER=$1
+fi
+if [ $# == 1 ] || [ $# == 2 ]; then
+  test $(/usr/local/sbin/userdomains ${USER}|wc -l) -ne 1 &&\
   echo "USAGE: $0 USER DOMAIN EMAIL" && exit 0 ||\
-  export DOMAIN=$(userdomains ${USER}) &&\
+  export DOMAIN=$(/usr/local/sbin/userdomains ${USER}) &&\
   echo "Found domain name ${DOMAIN}."
 
-  export USER=$1
+elif [ $# == 3 ]; then
+  export DOMAIN=$2
+  export EMAIL=$3
 
-  if [ $# == 3 ]; then
-    export DOMAIN=$3
-  elif [ $# == 1 ] || [ $# == 2]; then
-    export DOMAIN=$(/usr/local/sbin/userdomains ${USER})
-  fi  
-
-  if [ $# == 2 ]; then
-    export EMAIL=$2
-  elif [ $# == 1 ]; then
-    export EMAIL=${USER}@${DOMAIN}
-  fi 
-
-elif [ $# == 0 ] || [ $# >= 3 ]; then
+elif [ $# == 0 ] || [ $# > 3 ]; then
   echo ""
   read -e -p "Enter your cPanel username: " -i "" USER
-  export DOMAIN=$(/usr/local/sbin/userdomains ${USER})
+  test $(/usr/local/sbin/userdomains ${USER}|wc -l) -ne 1 &&\
+  echo "USAGE: $0 USER DOMAIN EMAIL" && exit 0 ||\
+  export DOMAIN=$(/usr/local/sbin/userdomains ${USER}) &&\
+  echo "Found domain name ${DOMAIN}."
   echo ""
   read -e -p "Enter your email address or press enter to accept the default: " -i "webmaster@${DOMAIN}" EMAIL
-  echo "Usage: $0 USER [EMAIL]"
+  echo "Usage: $0 USER [DOMAIN] [EMAIL]"
   exit
 fi
+
+
+elif [ $# == 3 ]; then
+  export EMAIL=$3
+elif [ $# == 2 ]; then
+  export EMAIL=$2
+elif [ $# == 1 ]; then
+  export EMAIL=${USER}@${DOMAIN}
+fi 
 
 export CRON="0 0 */60 * * /usr/local/sbin/letsencrypt-cpanel.sh ${USER} ${DOMAIN}"
 
