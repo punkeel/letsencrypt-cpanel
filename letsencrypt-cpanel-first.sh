80c3f02e3d5d0e3fecb9bc05e9ca8f8a78467928
Trey Brister <webstandardcss@gmail.com>
2015-12-11 21:41:29 +0000

    Issue #4 - derive domain name and email and reduce command arguments, need to test

--- letsencrypt-cpanel-first.sh
+++ letsencrypt-cpanel-first.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
-
-if [ $# != 2 ] && [ $# != 3 ]; then
-  echo "Usage: $0 USER DOMAIN"
-  echo "-or-"
-  echo "Usage: $0 USER DOMAIN EMAIL"
+if [ $# == 1 ] || [ $# == 2 ]; then
+  export USER=$1
+  export DOMAIN=$(/usr/local/bin/userdomains ${USER})
+  if [ $# == 2 ]; then
+    export EMAIL=$2
+  elif [ $# == 1 ]; then
+    export EMAIL=${USER}@${DOMAIN}
+  fi 
+elif [ $# == 0 ] || [ $# >= 3 ]; then
+  echo ""
+  read -e -p "Enter your cPanel username" -i "" USER
+  export DOMAIN=$(/usr/local/bin/userdomains ${USER})
+  echo ""
+  read -e -p "Enter your email address or press enter to accept the default" -i "webmaster@${DOMAIN}" EMAIL
+  echo "Usage: $0 USER [EMAIL]"
   exit
 fi
 
-export USER=$1
-export DOMAIN=$2
-export EMAIL=$3
 export CRON="0 0 */60 * * /usr/local/sbin/letsencrypt-cpanel.sh ${USER} ${DOMAIN}"
 
-if [ $# == 3 ]; then
-  echo "Using email address ${EMAIL}"
-  /root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
-
-elif [ $# == 2 ]; then
-  echo "Using email address webmaster@${DOMAIN}"
-  /root/letsencrypt/letsencrypt-auto --text --agree-tos --email webmaster@${DOMAIN} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
-fi
-
+echo "Using email address ${EMAIL}"
+/root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
 /root/installssl.pl ${DOMAIN}
 
 crontab -l | { cat; echo "${CRON}"; } | crontab -
