48fa36f1eb3a616ce48833e298989da526f3dd17
Trey Brister <webstandardcss@gmail.com>
2015-12-11 18:58:22 +0000

    debug to make three arguments work

--- letsencrypt-cpanel-first.sh
+++ letsencrypt-cpanel-first.sh
@@ -13,15 +13,17 @@ export DOMAIN=$2
 export EMAIL=$3
 export CRON="0 0 */60 * * /usr/local/sbin/letsencrypt-cpanel.sh ${USER} ${DOMAIN}"
 
-if [ $# == 2 ]; then
+
+if [ $# == 3 -a $# != 2 ]; then
+  echo "Using email address ${EMAIL}"
+  /root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
+fi
+
+if [ $# == 2 -a $# != 3 ]; then
   echo "Using email address webmaster@${DOMAIN}"
   /root/letsencrypt/letsencrypt-auto --text --agree-tos --email webmaster@${DOMAIN} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
 fi
 
-if [ $# == 3 ]; then
-  echo "Using email address ${EMAIL}"
-  /root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
-fi
 
 /root/installssl.pl ${DOMAIN}
 
