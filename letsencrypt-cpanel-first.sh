239cd0a4c64123fbd6b6697ad2cddb9c08529b5a
Trey Brister <webstandardcss@gmail.com>
2015-12-11 23:54:45 +0000

    refactor variable USER to USERNAME

--- letsencrypt-cpanel-first.sh
+++ letsencrypt-cpanel-first.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
 if [ $# == 1 ] || [ $# == 2 ] || [ $# == 3 ]; then
-  export USER=$1
+  export USERNAME=$1
 fi
 if [ $# == 1 ] || [ $# == 2 ]; then
-  test $(/usr/local/sbin/userdomains ${USER}|wc -l) -ne 1 &&\
-  echo "USAGE: $0 USER DOMAIN EMAIL" && exit 0 ||\
-  export DOMAIN=$(/usr/local/sbin/userdomains ${USER}) &&\
+  test $(/usr/local/sbin/userdomains ${USERNAME}|wc -l) -ne 1 &&\
+  echo "USAGE: $0 USERNAME DOMAIN EMAIL" && exit 0 ||\
+  export DOMAIN=$(/usr/local/sbin/userdomains ${USERNAME}) &&\
   echo "Found domain name ${DOMAIN}."
 
 elif [ $# == 3 ]; then
@@ -15,14 +15,14 @@ elif [ $# == 3 ]; then
 
 elif [ $# == 0 ] || [ $# > 3 ]; then
   echo ""
-  read -e -p "Enter your cPanel username: " -i "" USER
-  test $(/usr/local/sbin/userdomains ${USER}|wc -l) -ne 1 &&\
-  echo "USAGE: $0 USER DOMAIN EMAIL" && exit 0 ||\
-  export DOMAIN=$(/usr/local/sbin/userdomains ${USER}) &&\
+  read -e -p "Enter your cPanel username: " -i "" USERNAME
+  test $(/usr/local/sbin/userdomains ${USERNAME}|wc -l) -ne 1 &&\
+  echo "USAGE: $0 USERNAME DOMAIN EMAIL" && exit 0 ||\
+  export DOMAIN=$(/usr/local/sbin/userdomains ${USERNAME}) &&\
   echo "Found domain name ${DOMAIN}."
   echo ""
   read -e -p "Enter your email address or press enter to accept the default: " -i "webmaster@${DOMAIN}" EMAIL
-  echo "Usage: $0 USER [DOMAIN] [EMAIL]"
+  echo "Usage: $0 USERNAME [DOMAIN] [EMAIL]"
 fi
 
 
@@ -31,13 +31,13 @@ if [ $# == 3 ]; then
 elif [ $# == 2 ]; then
   export EMAIL=$2
 elif [ $# == 1 ]; then
-  export EMAIL=${USER}@${DOMAIN}
+  export EMAIL=${USERNAME}@${DOMAIN}
 fi 
 
-export CRON="0 0 */60 * * /usr/local/sbin/letsencrypt-cpanel.sh ${USER} ${DOMAIN}"
+export CRON="0 0 */60 * * /usr/local/sbin/letsencrypt-cpanel.sh ${USERNAME} ${DOMAIN}"
 
 echo "Using email address ${EMAIL}"
-/root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
+/root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USERNAME}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
 /root/installssl.pl ${DOMAIN}
 
 crontab -l | { cat; echo "${CRON}"; } | crontab -
