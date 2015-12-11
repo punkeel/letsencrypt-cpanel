2596d4655d846dd7ccb019cb3cd557a8b5bf72fc
Trey Brister <webstandardcss@gmail.com>
2015-12-11 14:01:16 -0600

    debug now you can customize the email address #1 is tested.

--- letsencrypt-cpanel-first.sh
+++ letsencrypt-cpanel-first.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
 
-if [ ! $# == 2 -o $# == 3 ]; then
+if [ $# != 2 ] && [ $# != 3 ]; then
   echo "Usage: $0 USER DOMAIN"
   echo "-or-"
   echo "Usage: $0 USER DOMAIN EMAIL"
@@ -13,17 +13,14 @@ export DOMAIN=$2
 export EMAIL=$3
 export CRON="0 0 */60 * * /usr/local/sbin/letsencrypt-cpanel.sh ${USER} ${DOMAIN}"
 
-
-if [ $# == 3 -a $# != 2 ]; then
+if [ $# == 3 ]; then
   echo "Using email address ${EMAIL}"
   /root/letsencrypt/letsencrypt-auto --text --agree-tos --email ${EMAIL} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
-fi
 
-if [ $# == 2 -a $# != 3 ]; then
+elif [ $# == 2 ]; then
   echo "Using email address webmaster@${DOMAIN}"
   /root/letsencrypt/letsencrypt-auto --text --agree-tos --email webmaster@${DOMAIN} certonly --renew-by-default --webroot --webroot-path /home/${USER}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
 fi
-
 
 /root/installssl.pl ${DOMAIN}
 
