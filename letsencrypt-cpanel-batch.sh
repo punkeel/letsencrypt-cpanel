0aa5300f52a979fb99e816306bafd90a9e1d7a31
Trey Brister <webstandardcss@gmail.com>
2015-12-12 11:45:32 +0000

    try to fix rate limiting

--- letsencrypt-cpanel-batch.sh
+++ letsencrypt-cpanel-batch.sh
@@ -3,10 +3,15 @@
 # This allows you to install SSL certificates for multiple usernames
 
 echo "Usage: letsencrypt-cpanel-batch USERNAME1 [USERNAME2 USERNAMEn]"
+echo "Rate limited issuing 10 certificates every three hours"
+echo "https://community.letsencrypt.org/t/quick-start-guide/1631"
 
 i=1
 for USERNAME
 do
+ #if [[ $(( i % 10 = 0 )) && $(( i >= 10 )) ]] # Sleep every ten certificates issued
+ #  then sleep 10860  # 3 hours and one minute
+ #done
  echo "Installing SSL Certificate for Username $((i++)) : ${USERNAME}"
- /usr/local/sbin/letsencrypt-cpanel ${USERNAME}
+ /usr/local/sbin/letsencrypt-cpanel ${USERNAME} # Register SSL certificate
 done
