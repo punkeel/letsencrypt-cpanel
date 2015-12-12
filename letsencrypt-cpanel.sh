869f09a5db778f71a7e3307fb1c5aa81f6ad1594
Trey Brister <webstandardcss@gmail.com>
2015-12-12 00:57:21 +0000

    debugging

--- letsencrypt-cpanel.sh
+++ letsencrypt-cpanel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
 
-if [ ! $# == 1 ] || [ ! $# == 2 ]; then
+if [ $# -ne 1 ] || [ $# -ne 2 ]; then
   echo ""
   read -e -p "Enter your cPanel username: " -i "" USERNAME
   test $(/usr/local/sbin/userdomains ${USERNAME}|wc -l) -ne 1 &&\
@@ -11,14 +11,14 @@ if [ ! $# == 1 ] || [ ! $# == 2 ]; then
   echo "Usage: $0 USERNAME [DOMAIN]"
 fi
 
-if [ ! $! == 0 ]; then
+if [ $! -ne 0 ]; then
   export USERNAME=$1
 fi
 
-if [ $# == 2 ]; then
+if [ $# -eq 2 ]; then
   export DOMAIN=$2
 
-elif [ $# == 1 ]; then
+elif [ $# -eq 1 ]; then
   test $(/usr/local/sbin/userdomains ${USERNAME}|wc -l) -ne 1 &&\
   echo "USAGE: $0 USERNAME DOMAIN" && exit 0 ||\
   export DOMAIN=$(/usr/local/sbin/userdomains ${USERNAME}) &&\
