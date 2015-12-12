4c5b7c242c52d76649d3b60d13fe24ec499b565b
Trey Brister <webstandardcss@gmail.com>
2015-12-12 00:36:20 +0000

    try to move enhancements from the letsencrypt-cpanel into this file

--- letsencrypt-cpanel.sh
+++ letsencrypt-cpanel.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 # https://forums.cpanel.net/threads/how-to-installing-ssl-from-lets-encrypt.513621/
 
-if [ ! $# == 2 ]; then
-  echo "Usage: $0 USERNAME DOMAIN"
-  exit
+elif [ $# == 0 ] || [ $# > 2 ]; then
+  echo ""
+  read -e -p "Enter your cPanel username: " -i "" USERNAME
+  test $(/usr/local/sbin/userdomains ${USERNAME}|wc -l) -ne 1 &&\
+  echo "USAGE: $0 USERNAME DOMAIN" && exit 0 ||\
+  export DOMAIN=$(/usr/local/sbin/userdomains ${USERNAME}) &&\
+  echo ""
+  echo "Usage: $0 USERNAME [DOMAIN]"
 fi
 
-export USERNAME=$1
-export DOMAIN=$2
+if [ $! != 0 ]; then
+  export USERNAME=$1
+fi
+
+if [ $# == 2 ]; then
+  export DOMAIN=$2
+fi
+
+if [ $# == 1 ]; then
+  test $(/usr/local/sbin/userdomains ${USERNAME}|wc -l) -ne 1 &&\
+  echo "USAGE: $0 USERNAME DOMAIN" && exit 0 ||\
+  export DOMAIN=$(/usr/local/sbin/userdomains ${USERNAME}) &&\
+fi
+
+echo "Found Domain Name ${DOMAIN}"
 
 /root/letsencrypt/letsencrypt-auto --text certonly --renew-by-default --webroot --webroot-path /home/${USERNAME}/public_html/ -d ${DOMAIN} -d www.${DOMAIN}
-~
+
 /root/installssl.pl ${DOMAIN} 
